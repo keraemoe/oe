fa3bc0e59f6c2b44debe3ebea71cddf1024a1f9b
keraemoe <weekera@gmail.com>
2023-03-10 21:05:02 +0600

    add: email send

--- src/Pages/Home/HomeComponents/Hero/Hero.jsx
+++ src/Pages/Home/HomeComponents/Hero/Hero.jsx
@@ -1,17 +1,50 @@
-﻿import React from 'react';
-import s from './Hero.module.scss'
+﻿import s from './Hero.module.scss'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { Button, message } from 'antd';
 
 const Hero = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_rnkeocu', 'template_cxwrf3v', form.current, 'rVxs1omlSkc0Q3S1P')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+    const [messageApi, contextHolder] = message.useMessage();
+    const key = 'updatable';
+    const openMessage = () => {
+        messageApi.open({
+            key,
+            type: 'loading',
+            content: 'Loading...',
+        });
+        setTimeout(() => {
+            messageApi.open({
+                key,
+                type: 'success',
+                content: 'Loaded!',
+                duration: 2,
+            });
+        }, 1000);
+    };
     return (
         <>
             <div id='container'>
                 <div className={s.hero}>
                     <h1>The easiest and fastest <br /> way to Invest in <br /> Agriculture</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu, mollis aenean <br /> sit dictum tincidunt. Ut arcu, suscipit ac etLorem ipsum dolor sit amet.Lorem <br /> ipsum dolor sit amet, consectetur </p>
-                    <div className={s.btn}>
-                        <input type="text" name="text" placeholder='Enter your email' id="" />
-                        <button type='button'>Get Started</button>
-                    </div>
+                    <form ref={form} onSubmit={sendEmail} className={s.btn}  >
+                        <input type="email" name="user_email" placeholder='Enter your email' />
+                        {contextHolder}
+                        <button value="send" type='submit' onClick={openMessage}>
+                            Send
+                        </button>
+                    </form>
                 </div>
             </div>
         </>
