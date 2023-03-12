b55bf3614773612e2d7eb2b823d5d6203148650a
keraemoe <weekera@gmail.com>
2023-03-12 14:04:03 +0600

    add: modal 50%

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,15 +1,37 @@
 ﻿import React, { useState, useEffect } from 'react';
-import { Descriptions, Avatar, Breadcrumb, Button, Modal } from 'antd';
-import { UserOutlined, HomeOutlined } from '@ant-design/icons';
+import {
+    Input,
+    Descriptions,
+    Avatar,
+    Button,
+    Modal,
+    Select,
+    Space,
+    Switch,
+    Cascader,
+    DatePicker,
+    Form,
+    InputNumber,
+    Radio,
+    TreeSelect,
+    Upload,
+} from 'antd';
+import {
+    CheckOutlined, InboxOutlined, UploadOutlined
+} from '@ant-design/icons';
 import s from './Profile.module.scss'
 import Header from '../Home/HomeComponents/Header/Header';
 import axios from 'axios';
 import { PROFILE } from "../../Constants/api"
 
+const { Option } = Select;
+
 const Profile = () => {
+
+    const { TextArea } = Input;
     const [open, setOpen] = useState(false);
     const [confirmLoading, setConfirmLoading] = useState(false);
-    const [modalText, setModalText] = useState('Content of the modal');
+    const [modalText, setModalText] = useState();
     const showModal = () => {
         setOpen(true);
     };
@@ -40,6 +62,29 @@ const Profile = () => {
         getProfile()
     }, [])
 
+    const [componentSize, setComponentSize] = useState('default');
+    const onFormLayoutChange = ({ size }) => {
+        setComponentSize(size);
+    };
+    const { Option } = Select;
+    const formItemLayout = {
+        labelCol: {
+            span: 6,
+        },
+        wrapperCol: {
+            span: 14,
+        },
+    };
+    const normFile = (e) => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e?.fileList;
+    };
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    };
     return (
         <>
             <Header />
@@ -57,18 +102,57 @@ const Profile = () => {
                             </Descriptions.Item>
                         </Descriptions>
                         <Button type="primary" onClick={showModal}>
-                            Open Modal with async logic
+                            Редактировать
                         </Button>
                         <Modal
                             className={s.modal}
                             zIndex={9000}
-                            title="Title"
+                            title="Профиль"
                             open={open}
                             onOk={handleOk}
                             confirmLoading={confirmLoading}
                             onCancel={handleCancel}
                         >
-                            <p>{modalText}</p>
+                            <Form
+                                labelCol={{
+                                    span: 4,
+                                }}
+                                wrapperCol={{
+                                    span: 14,
+                                }}
+                                layout="horizontal"
+                                initialValues={{
+                                    size: componentSize,
+                                }}
+                                onValuesChange={onFormLayoutChange}
+                                size={componentSize}
+                                style={{
+                                    maxWidth: 600,
+                                }}
+                            >
+                                <Form.Item label="Фото">
+                                    <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                                        <Upload.Dragger name="files" action="/upload.do">
+                                            <p className="ant-upload-drag-icon">
+                                                <InboxOutlined />
+                                            </p>
+                                            <p className="ant-upload-text">Нажмите или перетащите файл в эту область, чтобы загрузить</p>
+                                        </Upload.Dragger>
+                                    </Form.Item>
+                                </Form.Item>
+                                <Form.Item label="Имя">
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="Фамилия">
+                                    <Input />
+                                </Form.Item>
+                                <Form.Item label="О себе">
+                                    <TextArea rows={5} />
+                                </Form.Item>
+                                <Form.Item label="Актив" valuePropName="checked">
+                                    <Switch />
+                                </Form.Item>
+                            </Form>
                         </Modal>
                     </div>
                 </div>
